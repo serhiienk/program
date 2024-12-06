cd5cfbfdf914774b57e4feb8feb38d88be455a62
DENYS <despiteserg@gmail.com>
2024-12-06 16:12:22 +0100

    Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    // Поля для зберігання даних про дзвінки
+    public string LastName { get; set; }
+    public double CityCall { get; set; }
+    public double IntercityCall { get; set; }
+    public double InternationalCall { get; set; }
+
+}
+
+
